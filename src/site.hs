3ddd1e5fb0a9fab200fe5b2fefe3efd10237d36d
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-02-19 14:26:01 -0800

    bigger tag-cloud fonts

--- src/site.hs
+++ src/site.hs
@@ -208,7 +208,7 @@ wordListCompiler iden words = do
 
 siteCtx :: Tags -> Context String
 siteCtx tags = defaultContext
-  <> tagCloudField "tagcloud" 50 400 tags
+  <> tagCloudField "tagcloud" 90 400 tags
 
 recentCtx :: [Item String] -> Tags -> Context String
 recentCtx posts tags = constField "title" "Recent"
