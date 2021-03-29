f1195b9be0f7464bb5d5803451646ce250042c58
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-03-29 08:33:24 -0700

    publish.sh: warn on dirty repo

--- publish.sh
+++ publish.sh
@@ -8,5 +8,9 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 # Just in case, remove accidentally-added draft posts
 rm -rf .site/posts/2099 || true
 
+die() { echo "$@"; exit 1; }
+status="$(git status --porcelain)"
+[[ -z "$status" ]] || die "clean up git repo before publishing"
+
 # This picks up cryptoisland from your ~/.ssh/config
 rsync -rvz --delete .site/ cryptoisland:///var/www/cryptoisland/
